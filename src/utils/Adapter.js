ec4819c3f3728c2ac6e1dc83ca655fc530b5552c
Trevor Jameson <tjameson15@gmail.com>
2020-01-17 14:18:51 -0500

    Login post success. Setting jwt in sessionStorage

--- src/utils/Adapter.js
+++ src/utils/Adapter.js
@@ -1,6 +1,5 @@
 export default class Adapter {
 
-    // 
     static REQUEST_CONFIG = {
     HEADERS: {
         "Accepts": "application/json",
@@ -12,7 +11,7 @@ export default class Adapter {
     static post(endpoint, body) {
 
         // Determine whether JWT header should be included in request
-        // Deconstructing to avoid mutating cont
+        // deconstructing to avoid mutating container
         let headers = {...this.REQUEST_CONFIG.HEADERS};
         if (endpoint !== "login" && endpoint !== "signup") {
             headers.jwt = window.sessionStorage.jwt;
@@ -26,15 +25,18 @@ export default class Adapter {
     }
 
     static submitLoginOrSignupForm(e, endpoint, body) {
-    e.preventDefault();
-    
-    // Deconstruct body from state to avoid accidentally mutating
-    this.post(endpoint, { ...body })
-        .then(res => res.json())
-        .then(json => {
-        console.log(json);
-        // Note: ARCHITECTURE DECISION NEEDED. Should redux be used for project? See Trello board.
-        })
-        .catch(console.log);
+
+        e.preventDefault();
+        
+        // Deconstruct body from state to avoid accidentally mutating and
+        // nest under required user key
+        this.post(endpoint, { user: {...body} })
+            .then(res => res.json())
+            .then(json => {
+                // Note: ARCHITECTURE DECISION NEEDED. Should redux be used for project? See Trello board.
+                window.sessionStorage.setItem('jwt', json.jwt)
+                
+            })
+            .catch(console.log);
     };
 }
