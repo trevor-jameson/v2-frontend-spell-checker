139c4be4ecdcbbe4077ed56723ec8d3ffab6fc50
Trevor Jameson <github email address>
2020-02-08 10:41:28 -0500

    Babel & Enzyme config/dependencies added. First smoke test completion pending.

--- src/components/users/LandingPage.js
+++ src/components/users/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Login from './Login'
-import Signup from './Signup'
+import Signup from '../Signup'
 import Welcome from './Welcome'
 
 const StyledLandingContainer = styled.div`
