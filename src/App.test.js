55141da1492aedac46b665939a80a669642c3da1
Trevor Jameson <github email address>
2020-02-08 11:15:46 -0500

    Jest config working. First smoke test of <App/> passing.

--- src/App.test.js
+++ src/App.test.js
@@ -8,14 +8,14 @@ import store from './redux/store'
 
 // This is a "Smoke Test" to check whether the ReactDOM is able to render
 it('renders without crashing', () => {
-  const root = document.getElementById('root')
+  const root = document.createElement('div')
   ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>,
-    document.getElementById('root')
+    root
   );
     ReactDOM.unmountComponentAtNode(root);
 });
